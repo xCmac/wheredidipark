ecd5e0634cda5646b84273dffb0567f6456fbfa3
unknown <rachelvmadrigal@gmail.com>
2018-09-16 21:28:37 -0500

    Styled colors and color buttons

--- src/components/car/car.ts
+++ src/components/car/car.ts
@@ -1,19 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { Car } from '../../models/car';
 import { FloorProvider } from '../../providers/floor/floor';
+import { COLORS } from '../../theme/colors';
 
 @Component({
   selector: 'car',
   templateUrl: 'car.html'
 })
 export class CarComponent {
-  
   theCar: Car;
   carColor: string;
+  isOpen: boolean;
+  colors = COLORS;
 
   @Input('car') set car(car: Car) {
     this.theCar = car;
     this.carColor = this.getCarColor();
+    this.isOpen = false;
   }
 
   constructor(private floorProvider: FloorProvider) {
@@ -31,14 +34,30 @@ export class CarComponent {
 
   getCarColor() {
     if (!this.theCar.color) {
-      return "#FFFFFF";
+      return this.colors.darkGrey;
     } 
 
     switch(this.theCar.color) {
       case "red": {
-        return "#F69876";
+        return this.colors.red;
+      }
+      case "orange": {
+        return this.colors.orange;
+      }
+      case "yellow": {
+        return this.colors.yellow;
+      }
+      case "green": {
+        return this.colors.green;
+      }
+      case "blue": {
+        return this.colors.blue;
       }
     }
+  }
+
+  toggleIsOpen() {
+    this.isOpen = !this.isOpen;
   }
 
 }
